f702857d707329260310d499ed00e8a94505175c
Lukas8733 <171wita05@o365.htl-leoben.at>
2018-04-09 16:45:33 +0200

    testdaten angelegt

--- src/Parkplatz.java
+++ src/Parkplatz.java
@@ -1,9 +1,54 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Parkplatz {
 
 	public static void main(String[] args) {
+		// Testdaten einlesen
+		String input = "";
+		if (args.length > 0) {
+			String inputData = "";
+			inputData = loadInputData(args[0]);
+			if (!inputData.isEmpty()) {
+				input = inputData;
+			}
+		}
+		if (input.length() < 1) {
+			System.out.println("no input data found.");
+			System.exit(0);
+		}
 		
+			
 
 	}
 
+	public static String loadInputData(String datName) {
+		String fileData = "";
+		File file = new File(datName);
+
+		if (!file.canRead() || !file.isFile()) {
+			return "";
+		}
+		BufferedReader in = null;
+		try {
+			in = new BufferedReader(new FileReader(datName));
+			String zeile = null;
+			while ((zeile = in.readLine()) != null) {
+				fileData += zeile+"\r\n";
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (in != null)
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+		return fileData;
+	}	
+	
 }
